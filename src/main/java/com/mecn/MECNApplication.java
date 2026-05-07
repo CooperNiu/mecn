d3c180851207fad8a504a5d981e58859960eccae
CooperNiu <niuyanno.1@outlook.com>
2026-05-07 08:54:42 +0800

    feat: i18n基础设施 - MessageSource + I18nExceptionHandler + resource bundle

--- src/main/java/com/mecn/MECNApplication.java
+++ src/main/java/com/mecn/MECNApplication.java
@@ -2,7 +2,11 @@ package com.mecn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.ComponentScan;
+import com.mecn.i18n.ConsoleMessage;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 /**
  * MECN Spring Boot 应用启动类
@@ -16,12 +20,22 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = "com.mecn")
 public class MECNApplication {
 
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasenames("i18n/messages");
+        source.setDefaultEncoding("UTF-8");
+        source.setFallbackToSystemLocale(false);
+        return source;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MECNApplication.class, args);
+        ConsoleMessage msg = new ConsoleMessage();
         System.out.println("=================================================");
-        System.out.println("  MECN - 高维宏观经济因果网络联动模型");
-        System.out.println("  服务已启动：http://localhost:8080");
-        System.out.println("  API 文档：http://localhost:8080/api/docs");
+        System.out.println("  " + msg.get("app.name"));
+        System.out.println("  " + msg.fmt("app.started", "http://localhost:8080"));
+        System.out.println("  " + msg.fmt("app.docs", "http://localhost:8080/api/docs"));
         System.out.println("=================================================");
     }
 }
